391d6c26fbda7504ec22508b3d2c904263199010
Ilo <hei.iloniaina@gmail.com>
2025-07-31 09:20:24 +0300

    feat: q4 q5 q6

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-from fastapi import FastAPI, Response
+import datetime
+import base64
+from typing import List
+
+from fastapi import FastAPI, Response, Request, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-# @app.get("/")
-# def root():
-#     return {"message": "Hello World"}
-#
-#
-# @app.get("/hello/{name}")
-# def say_hello(name: str):
-#     return {"message": f"Hello {name}"}
+class PostModel(BaseModel):
+    author: str
+    title: str
+    content: str
+    creation_datetime: datetime.datetime
+
+
+posts_store: List[PostModel] = []
+
+
+def serialized_posts():
+    posts_converted = []
+    for one_post in posts_store:
+        posts_converted.append(one_post.model_dump())
+    return posts_converted
 
 
 @app.get("/ping")
 def ping():
     return Response(content="pong", media_type="text/plain", status_code=200)
+
+
+
 
 @app.get("/home")
 def get_home():
@@ -23,6 +38,28 @@ def get_home():
         html_content = f.read()
         return Response(content=html_content, status_code=200, media_type="text/html")
 
+
+@app.post("/posts")
+def post(event: list[PostModel]):
+    posts_store.extend(event)
+    return Response(status_code=201, content=str(serialized_posts()), media_type="application/json")
+
+
+@app.get("/posts")
+def get_all_posts():
+    return serialized_posts()
+
+
+@app.put("/posts")
+def update_or_add_posts(new_posts: list[PostModel]):
+    for new_post in new_posts:
+        for i, existing_post in enumerate(posts_store):
+            if existing_post.title == new_post.title:
+                posts_store[i] = new_post
+                break
+        else:
+            posts_store.append(new_post)
+    return serialized_posts()
 
 
 @app.get("/{full_path:path}")
